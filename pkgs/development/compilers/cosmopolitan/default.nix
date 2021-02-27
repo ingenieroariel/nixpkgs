a53da752a79d2e6008a4f00e55cd0c96e81405b3
Louis Bettens <louis@bettens.info>
2021-02-27 17:26:55 +0100

    fixup! cosmopolitan: init at 0.2

--- pkgs/development/compilers/cosmopolitan/default.nix
+++ pkgs/development/compilers/cosmopolitan/default.nix
@@ -1,20 +1,35 @@
 {
   lib,
   stdenv,
-  fetchzip,
+  fetchFromGitHub,
+  makeWrapper,
+  gcc,
 }:
 
 stdenv.mkDerivation rec {
   pname = "cosmopolitan";
   version = "0.2";
 
-  src = fetchzip {
-    url = "https://justine.lol/cosmopolitan/cosmopolitan-${version}.tar.gz";
+  src = fetchFromGitHub {
+    owner = "jart";
+    repo = "cosmopolitan";
+    rev = version;
     sha256 = "09bh3dpa6j1jxm9y4qgbmxz4spyn23gv1y0hlkdikcvfz1yvmgm4";
   };
+
+  nativeBuildInputs = [ makeWrapper ];
 
   preBuild = ''
     makeFlagsArray=(SHELL=/bin/sh "MKDIR=mkdir -p")
     rm test/libc/time/strftime_test.c  # segfaults
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/{bin,lib/include}
+    install o/cosmopolitan.h $out/lib/include
+    install o/cosmopolitan.a o/libc/crt/crt.o o/ape/ape.{o,lds} $out/lib
+    makeWrapper ${gcc}/bin/gcc $out/bin/cosmoc --add-flags "-O -static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone -fuse-ld=bfd -Wl,-T,$out/lib/ape.lds -include $out/lib/include/cosmopolitan.h $out/lib/{crt.o,ape.o,cosmopolitan.a}"
+    runHook postInstall
   '';
 }
